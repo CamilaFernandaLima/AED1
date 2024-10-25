d3745e28cd334a527651f99d01e4a2bb99c74fa6
CamilaFernandaLima <camila.fernanda@unifesp.br>
2024-10-24 23:53:36 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -7,52 +7,51 @@
 #define SIZE 9
 
 bool verifica(int matriz[SIZE][SIZE]) {
-    int existe[SIZE + 1]; // Rastrear números de 1 a 9
-	
+	int existe[SIZE + 1]; // Rastrear números de 1 a 9
 	int i,j;
 	
-    //Verifica linhas e colunas
-    for (i = 0; i < SIZE; i++) {
-        for (j = 1; j <= SIZE; j++){
-        	existe[j] = false;  //Inicializa os elementos com valor falso
+  //Verifica linhas e colunas
+  for (i = 0; i < SIZE; i++) {
+		for (j = 1; j <= SIZE; j++){
+			existe[j] = false;  //Inicializa os elementos com valor falso
 		} 
-        for (j = 0; j < SIZE; j++) {
-            if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
-                if (existe[matriz[i][j]]){
+		
+		for (j = 0; j < SIZE; j++) {
+			if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
+				if (existe[matriz[i][j]]){
 					return false;   // Duplicados
 				} else existe[matriz[i][j]] = true; // Marca como existente ao encontrar pela primeira vez
-            }
-        }   
-	
-
-    	for (i = 0; i < SIZE; i++){
-    		if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
-    			if (existe[matriz[j][i]]){
-    				return false;  //Duplicados
+			}
+		}   
+		
+		for (i = 0; i < SIZE; i++){
+			if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
+				if (existe[matriz[j][i]]){
+					return false;  //Duplicados
 				} else existe[matriz[i][j]] = true; // Marca como existente ao encontrar pela primeira vez
 			}
 		}
 	}
         
-    // Verifica submatrizes 3x3
-    for (int sub_i = 0; sub_i < 3; sub_i++) {
-        for (int sub_j = 0; sub_j < 3; sub_j++) {
-            for (int j = 1; j <= SIZE; j++) 
-				existe[j] = false;
+		// Verifica submatrizes 3x3
+		for (int sub_i = 0; sub_i < 3; sub_i++) {
+			for (int sub_j = 0; sub_j < 3; sub_j++) {
+				for (int j = 1; j <= SIZE; j++) 
+					existe[j] = false;
 				
-            for (int i = sub_i * 3; i < sub_i * 3 + 3; i++) {
-                for (int j = sub_j * 3; j < sub_j * 3 + 3; j++) {
-                    if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
-                        if (existe[matriz[i][j]]){
-							return false; // Duplicados
-                        } else existe[matriz[i][j]] = true; // Marca como existente ao encontrar pela primeira vez
-                    }
-                }
+				for (int i = sub_i * 3; i < sub_i * 3 + 3; i++) {
+					for (int j = sub_j * 3; j < sub_j * 3 + 3; j++) {
+						if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
+							if (existe[matriz[i][j]]){
+								return false; // Duplicados
+							} else existe[matriz[i][j]] = true; // Marca como existente ao encontrar pela primeira vez
             }
+          }
         }
+      }
     }
 
-    return true; // Se a matriz é válida
+		return true; // Se a matriz é válida
 }
         
 
@@ -71,10 +70,10 @@ int main() {
 			}
 		}       
 		
-        printf("Instancia %i\n", num);
-        if (verifica(matriz)) {
-            printf("SIM\n\n");
-        } else  printf("NAO\n\n");
+		printf("Instancia %i\n", num);
+		if (verifica(matriz)) {
+    	printf("SIM\n\n");
+    } else  printf("NAO\n\n");
 		
 		num++; 
 	}
