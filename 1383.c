8a4e9d4c2f5a8779dd12a7ca3241fc4334671c8c
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2024-10-29 14:22:31 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define SIZE 9
-
-bool verifica(int matriz[SIZE][SIZE]) {
-    int existe[SIZE + 1]; // Array para rastrear números de 1 a 9
+bool verifica(int matriz[9][9]) {
+    int existe[10]; // Array para rastrear números de 1 a 9
 
     // Verifica linhas e colunas
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < 9; i++) {
+	    
         // Verifica linha
-        for (int j = 1; j <= SIZE; j++) existe[j] = false;
-        for (int j = 0; j < SIZE; j++) {
+        for (int j = 1; j <= 9; j++) existe[j] = false;
+        for (int j = 0; j < 9; j++) {
             if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
                 if (existe[matriz[i][j]]) return false; // Duplicado
                 existe[matriz[i][j]] = true; // Marca como existente
@@ -18,8 +17,8 @@ bool verifica(int matriz[SIZE][SIZE]) {
         }
 
         // Verifica coluna
-        for (int j = 1; j <= SIZE; j++) existe[j] = false;
-        for (int j = 0; j < SIZE; j++) {
+        for (int j = 1; j <= 9; j++) existe[j] = false;
+        for (int j = 0; j < 9; j++) {
             if (matriz[j][i] >= 1 && matriz[j][i] <= 9) {
                 if (existe[matriz[j][i]]) return false; // Duplicado
                 existe[matriz[j][i]] = true; // Marca como existente
@@ -28,11 +27,11 @@ bool verifica(int matriz[SIZE][SIZE]) {
     }
 
     // Verifica submatrizes 3x3
-    for (int sub_i = 0; sub_i < 3; sub_i++) {
-        for (int sub_j = 0; sub_j < 3; sub_j++) {
-            for (int j = 1; j <= SIZE; j++) existe[j] = false;
-            for (int i = sub_i * 3; i < sub_i * 3 + 3; i++) {
-                for (int j = sub_j * 3; j < sub_j * 3 + 3; j++) {
+    for (int subi = 0; subi < 3; subi++) {
+        for (int subj = 0; subj < 3; subj++) {
+            for (int j = 1; j <= 9; j++) existe[j] = false;
+            for (int i = subi * 3; i < subi * 3 + 3; i++) {
+                for (int j = subj * 3; j < subj * 3 + 3; j++) {
                     if (matriz[i][j] >= 1 && matriz[i][j] <= 9) {
                         if (existe[matriz[i][j]]) return false; // Duplicado
                         existe[matriz[i][j]] = true; // Marca como existente
@@ -50,23 +49,23 @@ int main() {
     scanf("%i", &n);
 
     int num = 1;
-	while(num<=n){
-		int matriz[9][9];
-		int i,j;
+    while(num<=n){
+        int matriz[9][9];
+	int i,j;
 		
-		for (i=0; i<9; i++){
-			for (j=0; j<9; j++){
-				scanf ("%i", &matriz[i][j]);
-			}
-		}       
+	for (i=0; i<9; i++){
+	    for (j=0; j<9; j++){
+		scanf ("%i", &matriz[i][j]);
+	    }
+	}       
 		
         printf("Instancia %i\n", num);
         if (verifica(matriz)) {
             printf("SIM\n\n");
         } else  printf("NAO\n\n");	
 		
-		num++; 
-	}
+	num++; 
+    }
 	
-	return 0;
+    return 0;
 }
