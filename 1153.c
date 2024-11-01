648698180cba3ebe080bac145e9e0500093ab40b
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2024-11-01 17:36:01 -0300

    Create 1153.c

---
+++ 1153.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int fat(n){
+
+    int fatorial;
+    fatorial = n;
+
+    if(0<n<13){
+        for (int i=1; i<n; i++){
+            fatorial = fatorial * (n-i);
+        } 
+    }
+
+    return fatorial;
+}
+
+int main(){
+    int n, f;
+    scanf("%i", &n);
+
+    f = fat(n);
+    printf("%i\n", f);
+
+    return 0;
+}
