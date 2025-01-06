990f10b07fd97fa9f6f1cdec2b731340aca56de2
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2025-01-05 21:45:04 -0300

    Create 2374.c

---
+++ 2374.c
@@ -0,0 +1,15 @@
+//Camila Fernanda e Silva Lima - RA: 176.165
+//Pneu
+
+#include <stdio.h>
+
+int main(){
+    int n, m;
+
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    printf("%d\n", n-m);
+
+    return 0;
+}
