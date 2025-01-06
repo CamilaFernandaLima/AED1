4a73911d115d6cd5e4b2503cb92eb2750cba9568
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2025-01-05 21:04:55 -0300

    Create 1003.c

---
+++ 1003.c
@@ -0,0 +1,15 @@
+//Camila Fernanda e Silva Lima - RA: 176.165
+//Soma Simples
+
+#include <stdio.h>
+
+int main(){
+    int x, y, SOMA;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    SOMA = x+y;
+
+    printf("SOMA = %d\n", SOMA);
+}
