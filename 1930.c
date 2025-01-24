89eb4eb5fe6552656db278e335122ae740c4fc72
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2025-01-24 09:32:24 -0300

    Create 1930.c

---
+++ 1930.c
@@ -0,0 +1,15 @@
+//Camila Fernanda e Silva Lima - RA: 176165
+//Tomadas
+
+#include <stdio.h>
+
+int main(){
+    int t1, t2, t3, t4;
+
+    scanf("%d %d %d %d", &t1, &t2, &t3, &t4);
+    int soma;
+    soma = t1 + (t2-1) + (t3-1) + (t4-1);
+    printf("%d\n", soma);
+
+    return 0;
+}
